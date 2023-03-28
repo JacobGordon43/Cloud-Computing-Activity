d6479f700fcd3bf837da046742f824dfd0628a97
JacobGordon43 <77366005+JacobGordon43@users.noreply.github.com>
2023-03-28 00:20:36 -0700

    Update SecurityBusinessService.java
    Added logger

--- src/main/java/com/gcu/business/SecurityBusinessService.java
+++ src/main/java/com/gcu/business/SecurityBusinessService.java
@@ -1,11 +1,13 @@
 package com.gcu.business;
 
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @Service
 public class SecurityBusinessService {
+	Logger logger = LoggerFactory.getLogger(SecurityBusinessService.class);
     public boolean authenticate(String username, String password){
-        System.out.println(String.format("Hello from the SecurityBusinessService with a username of %s and a password of %s", username, password));
+    	logger.info(String.format("You've been authenticated in the SecurityBusinessService with a username of %s and a password of %s", username, password));
         return true;
     }
 }
